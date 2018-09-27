aabf9150433eab2ee3d07d5a645e6c8b07b51702
Daniel Munoz <daniel.munozdev@gmail.com>
2018-09-27 16:45:10 -0700

    add start files

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # dynamic-components
 A website about dynamic Vue components
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="container">
+    <p>Dynamic Components</p>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+  html {
+    box-sizing: border-box;
+  }
+
+  *,
+  *:before,
+  *:after {
+    box-sizing: inherit;
+  }
+
+  body {
+    font-family: sans-serif;
+    line-height: 1.5;
+    margin: 0;
+  }
+
+  .container {
+    margin: 0 auto;
+    max-width: 1360px;
+  }
+</style>
