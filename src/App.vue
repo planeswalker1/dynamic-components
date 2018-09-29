4ef68f20ec6c681b206940c194b957b7ed594323
Daniel Munoz <daniel.munozdev@gmail.com>
2018-09-28 20:24:04 -0700

    add main and form styles

--- src/App.vue
+++ src/App.vue
@@ -70,5 +70,163 @@ export default {
   .container {
     margin: 0 auto;
     max-width: 1360px;
+    padding: 40px;
+    width: 60%;
+  }
+
+  /* link styles */
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    list-style-type: none;
+    padding: 0;
+    width: 100%;
+  }
+
+  li {
+    margin-right: 10px;
+  }
+
+  a {
+    background-color: transparent;
+    color: #007bff;
+    text-decoration: none;
+  }
+
+  a:hover {
+    color: #0056b3;
+    text-decoration: underline;
+  }
+
+  h1,
+  form {
+    margin: 0 auto;
+    max-width: 600px;
+  }
+
+  .heading {
+    margin: 0;
+    max-width: unset;
+  }
+
+  h1 {
+    margin-top: 1em;
+  }
+
+  p {
+    margin-bottom: 0;
+  }
+
+  .form__header,
+  .form__fields,
+  .form__controls {
+    padding: 5px;
+  }
+
+  /* form control styles */
+  .group {
+    margin-bottom: 10px;
+  }
+
+  label {
+    display: inline-block;
+    margin: .5em 0;
+  }
+
+  input {
+    background-color: #fff;
+    background-clip: padding-box;
+    border: 1px solid #ced4da;
+    border-radius: .25em;
+    color: #495057;
+    display: block;
+    font-size: 1em;
+    width: 300px;
+    line-height: 1.5;
+    padding: .375em .75em;
+    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+  }
+
+  input:focus {
+    background-color: #fff;
+    border-color: #80bdff;
+    box-shadow: 0 0 0 0.2em rgba(0,123,255,.25);
+    color: #495057;
+    outline: 0;
+  }
+
+  textarea {
+    background-clip: padding-box;
+    background-color: #fff;
+    border: 1px solid #ced4da;
+    border-radius: .25em;
+    color: #495057;
+    display: block;
+    line-height: 1.5;
+    overflow: auto;
+    padding: .375em .75em;
+    resize: vertical;
+    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    width: 300px;
+  }
+
+  textarea:focus {
+    background-color: #fff;
+    border-color: #80bdff;
+    box-shadow: 0 0 0 0.2em rgba(0,123,255,.25);
+    color: #495057;
+    outline: 0;
+  }
+
+  button {
+    background-color: #007bff;
+    border: 1px solid transparent;
+    border-color: #007bff;
+    border-radius: .25em;
+    color: #fff;
+    display: inline-block;
+    font-size: 1em;
+    font-weight: 400;
+    line-height: 1.5;
+    padding: .375em .75em;
+    text-align: center;
+    transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+    user-select: none;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    vertical-align: middle;
+    white-space: nowrap;
+  }
+
+  button:hover {
+    background-color: #0069d9;
+    border-color: #0062cc;
+    color: #fff;
+    cursor: pointer;
+  }
+
+  button:focus {
+    outline: 0;
+    box-shadow: 0 0 0 0.2em rgba(0,123,255,.5);
+  }
+
+  @media screen and (max-width: 800px) {
+    .container {
+      padding: 20px;
+      width: 80%;
+    }
+  }
+
+  @media screen and (max-width: 500px) {
+    .container {
+      padding: 10px;
+      width: 90%;
+    }
+
+    input,
+    textarea {
+      width: 100%;
+    }
   }
 </style>
